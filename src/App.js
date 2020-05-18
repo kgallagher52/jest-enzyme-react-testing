d364cfa2b2fd6b427915943aea756dc66cffbc59
keatongallagher <keatongallagher@its-MacBook-Pro.local>
2020-05-18 10:20:27 -0600

    Added some style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       // Using data-test because it is used specific to test
-      <div data-test="component-app">
+      <div className="app-container" data-test="component-app">
         <h1 data-test="counter-display">The counter is currently ...</h1>
         <button data-test="increment-button">increment counter</button>
       </div>
