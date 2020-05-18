aa23c024cacf4d5eb05f9caf9f6f330b0ddd4c33
keatongallagher <keatongallagher@its-MacBook-Pro.local>
2020-05-18 11:41:09 -0600

    Created the state and the test that checks the counter is initially at zero

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      counter: 0
+    }
+  }
   render() {
     return (
       // Using data-test because it is used specific to test
