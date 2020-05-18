66e5078c4ee1d11ce4650c4f0df01154fa3f7df3
keatongallagher <keatongallagher@its-MacBook-Pro.local>
2020-05-18 13:18:07 -0600

    Fully tested counter app

--- src/App.js
+++ src/App.js
@@ -13,8 +13,13 @@ class App extends Component {
     return (
       // Using data-test because it is used specific to test
       <div className="app-container" data-test="component-app">
-        <h1 data-test="counter-display">The counter is currently ...</h1>
-        <button data-test="increment-button">increment counter</button>
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        <button 
+        data-test="increment-button"
+        onClick={() => this.setState({ counter: this.state.counter + 1})}
+        >
+          increment counter
+        </button>
       </div>
     )
   }
